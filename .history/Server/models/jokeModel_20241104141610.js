3421927df5a35e578f621e4f1001252db622d42a
elchanan chen <elchananc79@gmail.com>
2024-11-05 15:57:27 +0200

    mbjm

---
+++ .history/Server/models/jokeModel_20241104141610.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const jokeSchema = new mongoose.Schema({
+  joke: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  creatadBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Joke", jokeSchema);
