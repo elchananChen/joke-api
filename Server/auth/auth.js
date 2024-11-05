3f80f06f5a282ddab4fdd7576ac7ac8b607e53f7
Nathan Kilinski <nathkilin@gmail.com>
2024-11-05 15:48:40 +0200

    s

--- Server/auth/auth.js
+++ Server/auth/auth.js
@@ -2,4 +2,8 @@ import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
 async function signIn(password, mongoPassword){
 const combinedPassword = password+process.env.ENCYPTION_SECRET
+console.log(combinedPassword);
+
+
     }
+signIn(1344, 43231)
