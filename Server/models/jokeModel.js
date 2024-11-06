3553edd01b9cfb53db3c868322324e3178d6fd83
Nathan Kilinski <nathkilin@gmail.com>
2024-11-06 14:47:34 +0200

    twoline

--- Server/models/jokeModel.js
+++ Server/models/jokeModel.js
@@ -1,16 +1,31 @@
-import mongoose from "mongoose";
+import mongoose, { set } from "mongoose";
+
+const isTwoStepsJoke = (input) => {
+  if (this.category === 'Knock-Knock Jokes') {
+    required : true
+  }
+}
 
 const jokeSchema = new mongoose.Schema({
-  joke: {
-    type: String,
-    required: true,
-  },
+
   content: {
     type: String,
-    required: true,
+    validate: {
+    } 
+  },
+  setUp: {
+    type: String,
+    validate: {
+      validator: isTwoStepsJoke()
+    }
+  },
+  punchline: {
+    type: String,
   },
   category: {
     type: String,
+    required: true,
+    enum: ['Knock-Knock Jokes', 'Puns', 'One-liners', 'Dad Jokes', 'Dark Humor', 'Jews Jokes']
   },
   creatadBy: {
     type: mongoose.Schema.Types.ObjectId,
